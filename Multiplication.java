483c5a953f12ee52b35dbd3babcf6d2dbbb17a48
Mani <106660208+Mani10101@users.noreply.github.com>
2022-10-02 22:02:25 +0530

    import java.util.Scanner; class mult {     public static void main(String args[])     {     int n,m;     Scanner sc = new Scanner(System.in);     n=sc.nextInt();     m=sc.nextInt();     System.out.println(n*m);     } }

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class mult {
+    public static void main(String args[])
+    {
+    int n,m;
+    Scanner sc = new Scanner(System.in);
+    n=sc.nextInt();
+    m=sc.nextInt();
+    System.out.println(n*m);
+    }
+}
